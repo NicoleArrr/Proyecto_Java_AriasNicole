5cf486f7239d73f9e15227418a9539e5c03619a7
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2026-02-06 07:34:54 -0500

    feat :sparkles: added marca class

--- src/main/java/MODELO/marca.java
+++ src/main/java/MODELO/marca.java
@@ -2,5 +2,27 @@
 package MODELO;
 
 public class marca {
+    int id;
+    String nombre;
+
+    public marca(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }    
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
