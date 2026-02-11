3654be8ec16ce559920cb961d874009fdc07f935
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2026-02-10 21:12:55 -0500

    fix :hammer: import validaciones class fixed

--- src/main/java/VISTA/TecnoStore.java
+++ src/main/java/VISTA/TecnoStore.java
@@ -9,7 +9,7 @@ public class TecnoStore {
     public static void main(String[] args) {
         Conexion c = new Conexion();
         c.conectar();
-        Validaciones ve = new Validaciones;
+        Validaciones ve = new Validaciones();
         
         System.out.println("""
                             *********************
